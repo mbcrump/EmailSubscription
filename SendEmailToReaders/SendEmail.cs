f5833493fe83f1c9595dbda8c8b2e637a033d450
Michael Crump <mbcrump29@gmail.com>
2019-02-17 20:57:04 -0800

    fix bug

--- SendEmailToReaders/SendEmail.cs
+++ SendEmailToReaders/SendEmail.cs
@@ -21,6 +21,7 @@ namespace SendEmailToReaders
     {
         [FunctionName("SendEmail")]
         public static async Task Run([TimerTrigger("0 30 9 * * SUN")]TimerInfo myTimer, TraceWriter log)
+       // public static async Task Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, TraceWriter log)
         {
 
             string feedurl = "https://www.michaelcrump.net/feed.xml";
@@ -28,7 +29,7 @@ namespace SendEmailToReaders
 
             XmlReader reader = XmlReader.Create(feedurl);
             SyndicationFeed feed = SyndicationFeed.Load(reader);
-            
+
 
             last7days = last7days + "<b>New updates in the last 7 days:</b><br><br>";
             foreach (SyndicationItem item in feed.Items)
@@ -36,13 +37,15 @@ namespace SendEmailToReaders
                 if ((DateTime.Now - item.PublishDate).TotalDays < 8)
                 {
                     last7days = last7days + "<a href=\"" + item.Links[0].Uri + "\">" + item.Title.Text + "</a><br>";
-                }       
+                }
             }
 
-            
+
             reader.Close();
 
-            feedurl = "https://www.youtube.com/feeds/videos.xml?playlist_id=PLLasX02E8BPCNCK8Thcxu-Y-XcBUbhFWC";
+            feedurl = "https://podsync.net/Z1F8nb1tN";
+            //original youtube playlist feed url
+            //feedurl = "https://www.youtube.com/feeds/videos.xml?playlist_id=PLLasX02E8BPCNCK8Thcxu-Y-XcBUbhFWC";
             XmlReader reader1 = XmlReader.Create(feedurl);
             SyndicationFeed feed1 = SyndicationFeed.Load(reader1);
             
@@ -73,7 +76,12 @@ namespace SendEmailToReaders
             client.Credentials = new System.Net.NetworkCredential(ConfigurationManager.AppSettings["SendGridUserName"], ConfigurationManager.AppSettings["SendGridSecret"]);
 
             MailMessage mail = new MailMessage();
+
             List<string> recipientlist = GetAllEmailAddresses(table);
+            //comment out for prod
+            //List<string> recipientlist = new List<string>();
+            //recipientlist.Add("mbcrump29@gmail.com");
+            //
             header.SetTo(recipientlist);
             mail.From = new MailAddress("michael@michaelcrump.net", "MichaelCrump.net");
             mail.To.Add("no-reply@michaelcrump.net");
