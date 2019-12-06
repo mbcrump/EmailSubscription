3ee15dccb957f81848091b8d9d6f1fee5b43be4d
Michael Crump <mbcrump29@gmail.com>
2019-12-06 12:18:51 -0800

    add developer streams and social

--- SendEmailToReaders/SendEmail.cs
+++ SendEmailToReaders/SendEmail.cs
@@ -1,27 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Xml;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
-using SendGrid;
-using SendGrid.Helpers.Mail;
-using System.ServiceModel.Syndication;
-using System.Linq;
-using System.Configuration;
-using Microsoft.WindowsAzure.Storage.Table;
 using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
 using SendGrid.SmtpApi;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Net.Mail;
+using System.ServiceModel.Syndication;
 using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
 
 namespace SendEmailToReaders
-{
+{           //dfasdf
     public static class SendEmail
     {
         [FunctionName("SendEmail")]
-      public static async Task Run([TimerTrigger("0 30 9 * * SUN")]TimerInfo myTimer, TraceWriter log)
-       //public static async Task Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, TraceWriter log)
+       public static async Task Run([TimerTrigger("0 30 9 * * SUN")]TimerInfo myTimer, TraceWriter log)
+        //public static async Task Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, TraceWriter log)
         {
 
 
@@ -33,35 +30,52 @@ namespace SendEmailToReaders
             SyndicationFeed feed = SyndicationFeed.Load(reader);
 
 
-            last7days = last7days + "<b>New updates in the last 7 days:</b><br><br>";
+            last7days = last7days + "<b>Latest updates in the past 7 days:</b><br><br>";
             foreach (SyndicationItem item in feed.Items)
             {
                 if ((DateTime.Now - item.PublishDate).TotalDays < 8)
                 {
-                    last7days = last7days + "<a href=\"" + item.Links[0].Uri + "\">" + item.Title.Text + "</a><br>";
+                    last7days = last7days + "<a href=\"" + item.Links[0].Uri + "\">" + "Azure Tips and Tricks: " + item.Title.Text + "</a><br>";
                 }
             }
 
 
             reader.Close();
 
-          
+
             feedurl = "http://fetchrss.com/rss/5cb90f018a93f83d098b45675cb90ed88a93f829098b4567.xml";
             //original youtube playlist feed url
             //feedurl = "https://www.youtube.com/feeds/videos.xml?playlist_id=PLLasX02E8BPCNCK8Thcxu-Y-XcBUbhFWC";
             XmlReader reader1 = XmlReader.Create(feedurl);
             SyndicationFeed feed1 = SyndicationFeed.Load(reader1);
-            
-            
+
+
             foreach (SyndicationItem item in feed1.Items)
             {
                 if ((DateTime.Now - item.PublishDate).TotalDays < 8)
                 {
-                    last7days = last7days + "<a href=\"" + item.Links[0].Uri + "\">" + "Azure Tips and Tricks Videos - " + item.Title.Text.Replace(" | Azure Tips and Tricks", "") + " </a><br>";
+                    last7days = last7days + "<a href=\"" + item.Links[0].Uri + "\">" + "Azure Tips and Tricks Video: " + item.Title.Text.Replace(" | Azure Tips and Tricks", "") + " </a><br>";
                 }
             }
 
             reader1.Close();
+
+            feedurl = "http://fetchrss.com/rss/5cb90f018a93f83d098b45675deaae3f8a93f8ac308b4567.xml";
+            XmlReader reader2 = XmlReader.Create(feedurl);
+            SyndicationFeed feed2 = SyndicationFeed.Load(reader2);
+
+
+            foreach (SyndicationItem item in feed2.Items)
+            {
+                if ((DateTime.Now - item.PublishDate).TotalDays < 8)
+                {
+                    last7days = last7days + "<a href=\"" + item.Links[0].Uri + "\">" + "Azure Developer Streams Video: " + item.Title.Text.Replace(" | Azure Developer Streams", "") + " </a><br>";
+                }
+            }
+
+            reader2.Close();
+
+            last7days = last7days + "<br><br>Follow me on <a href=\"https://twitter.com/mbcrump\">" + "Twitter</a> for daily developer news or on " + "<a href=\"https://twitch.tv/mbcrump\">" + "Twitch</a> for live coding sessions";
 
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["TableStorageConnString"]);
             CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
